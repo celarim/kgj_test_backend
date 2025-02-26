f81ec9f47147b998f5ecc7c2c7d3ffc976893fbb
celarim <k6jun@kookmin.ac.kr>
2025-02-26 09:30:26 +0900

    [Init] Initialize Project
    PR, Commit 컨벤션 추가
    필요 라이브러리 추가

---
+++ src/main/java/com/example/kgj_test_backend/KgjTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kgj_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgjTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KgjTestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kgj_test_backend/KgjTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kgj_test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgjTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
