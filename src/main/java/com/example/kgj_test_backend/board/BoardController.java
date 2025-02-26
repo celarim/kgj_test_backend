8ffac848a4bf414d8ee92fbfc7fa91b57cebf9df
celarim <k6jun@kookmin.ac.kr>
2025-02-26 10:37:47 +0900

    [Feat] Board 작성 Swagger 작성
    테스트를 위한 Swagger 작성
    테스트 성공

--- src/main/java/com/example/kgj_test_backend/board/BoardController.java
+++ src/main/java/com/example/kgj_test_backend/board/BoardController.java
@@ -3,6 +3,7 @@ package com.example.kgj_test_backend.board;
 import com.example.kgj_test_backend.board.model.Board;
 import com.example.kgj_test_backend.board.model.BoardDto;
 import com.example.kgj_test_backend.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
     private final BoardService boardService;
 
+    @Operation(summary = "게시글 작성", description = "제목, 내용, 작성자를 입력받아 게시글을 DB에 저장")
     @PostMapping("/create")
     public ResponseEntity<BaseResponse<Long>> createBoard(@RequestBody BoardDto.BoardRegisterReq dto) {
         Long idx = boardService.addBoard(dto);
--- src/main/java/com/example/kgj_test_backend/board/model/BoardDto.java
+++ src/main/java/com/example/kgj_test_backend/board/model/BoardDto.java
@@ -1,9 +1,16 @@
 package com.example.kgj_test_backend.board.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
 public class BoardDto {
+    @Getter
     public static class BoardRegisterReq {
+        @Schema(example = "게시글 1")
         private String title;
+        @Schema(example = "게시글 내용")
         private String content;
+        @Schema(example = "작성자 1")
         private String writer;
 
         public Board toEntity() {
