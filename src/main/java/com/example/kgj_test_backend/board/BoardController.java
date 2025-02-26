9234be3292433905b7a0fa926951770321773e27
celarim <k6jun@kookmin.ac.kr>
2025-02-26 10:25:39 +0900

    [Feat] Board 작성 컨트롤러 제작
    컨트롤러 작성

--- src/main/java/com/example/kgj_test_backend/board/BoardController.java
+++ src/main/java/com/example/kgj_test_backend/board/BoardController.java
@@ -1,8 +1,12 @@
 package com.example.kgj_test_backend.board;
 
+import com.example.kgj_test_backend.board.model.Board;
+import com.example.kgj_test_backend.board.model.BoardDto;
+import com.example.kgj_test_backend.common.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,8 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("/create")
-    public ResponseEntity<Long> create
+    public ResponseEntity<BaseResponse<Long>> createBoard(@RequestBody BoardDto.BoardRegisterReq dto) {
+        Long idx = boardService.addBoard(dto);
+        return ResponseEntity.ok(BaseResponse.success(idx));
+    }
 }
